08d48db55c19db106ecbf74f04288c204c4962a1
Alessandro Ronchi <aleron75@gmail.com>
2013-10-24 17:21:39 +0200

    added a comment and changed notification background

--- Mageploy/Model/Observer.php
+++ Mageploy/Model/Observer.php
@@ -4,18 +4,23 @@
  *
  * @author Alessandro Ronchi <aronchi at webgriffe.com>
  */
-class PugMoRe_Mageploy_Model_Observer {
+class PugMoRe_Mageploy_Model_Observer
+{
 
-    public function observeRequest($observer) {
+    /**
+     * Observes the controller_action_predispatch event
+     */
+    public function observeRequest($observer)
+    {
         $helper = Mage::helper('pugmore_mageploy');
         if (!$helper->isActive()) {
             return;
         }
-        
+
         $request = Mage::app()->getRequest();
-        $funnel = Mage::getModel('pugmore_mageploy/request_funnel')
-                ->init(Mage::getSingleton('pugmore_mageploy/io_file'))
-                ->dispatch($request);
+        Mage::getModel('pugmore_mageploy/request_funnel')
+            ->init(Mage::getSingleton('pugmore_mageploy/io_file'))
+            ->dispatch($request);
     }
 
 }
